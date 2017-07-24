007a9ca570771402d24bb1c9645023a02acf6460
Youhei Yamaji <yamaji@solis.t.u-tokyo.ac.jp>
2017-07-24 12:54:09 +0900

    build-hihi-for-eccs.sh is updated

--- build-hphi-for-eccs.sh
+++ build-hphi-for-eccs.sh
@@ -13,7 +13,7 @@ patch -p1 < ../HPhi-2.0.0.patch
 # cmake & build & install
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$HOME/HPhi -DCMAKE_C_COMPILER=gcc-5 -DCMAKE_Fortran_COMPILER=gfortran-5 ..
+cmake -DCMAKE_INSTALL_PREFIX=$HOME/HPhi -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_Fortran_COMPILER=gfortran-7 ..
 make
 make install
 
